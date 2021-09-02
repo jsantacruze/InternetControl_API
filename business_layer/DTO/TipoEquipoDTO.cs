ca23e88a4a68f91cd7eab2c73a5af97374b976c1
Tito Guasco <titus13.11@gmail.com>
2021-09-02 00:32:21 -0500

    tito

---
+++ business_layer/DTO/TipoEquipoDTO.cs
@@ -0,0 +1,9 @@
+namespace business_layer.DTO
+{
+    public class TipoEquipoDTO
+    {
+        public string IdtipoEquipo { get; set; }
+        public string DescripcionTipo { get; set; }
+        public string ObservacionesTipo { get; set; }
+    }
+}
