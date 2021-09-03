26de3a7215e4f64979c1cd6fd6952c7537cc8fe4
jsantacruze <86815142+jsantacruze@users.noreply.github.com>
2021-09-03 15:13:13 -0500

    Jhovany

--- business_layer/MappingProfile.cs
+++ business_layer/MappingProfile.cs
@@ -12,9 +12,9 @@ namespace business_layer
             CreateMap<Suscriptor, SuscriptorDTO>();
             CreateMap<Ciudad,CiudadDTO>();
             CreateMap<Sexo,SexoDTO>();
-            CreateMap<TrackingSuscripcion, IncidenciaDTO>();
-            /*.ForMember(destino => destino.incidencia_id, opt => opt.MapFrom(src => src.Idtracking))
-            .ForMember(destino => destino.EmpleadoAsignado, opt => opt.MapFrom(src => src.IdempleadoAsignadoNavigation))
+            CreateMap<TrackingSuscripcion, IncidenciaDTO>()
+            .ForMember(destino => destino.incidencia_id, opt => opt.MapFrom(src => src.Idtracking));
+            /*.ForMember(destino => destino.EmpleadoAsignado, opt => opt.MapFrom(src => src.IdempleadoAsignadoNavigation))
             .ForMember(destino => destino.UsuarioCrea, opt => opt.MapFrom(src => src.IdusuarioCreaNavigation))
             .ForMember(destino => destino.Suscripcion, opt => opt.MapFrom(src => src.Suscripcion));*/
         }
