985788908440be55afd67fb92f504155b7c31e3e
Yandi <34551585+yanditv@users.noreply.github.com>
2022-01-05 18:22:34 -0500

    JUNIOR

--- business_layer/DTO/SuscriptorDTO.cs
+++ business_layer/DTO/SuscriptorDTO.cs
@@ -12,7 +12,10 @@ namespace business_layer.DTO
         public string StrIdsexo { get; set; }
         public string StrTelefono { get; set; }
         public string StrMovil { get; set; }
-        public string StrEmail { get; set; }
-
+        public byte[] ImgFoto { get; set; }
+        public bool BlnActivo { get; set; }
+        public bool BlnPersonaNatural { get; set; }
+        public double DblPorcentajeDescuento { get; set; }
+        public string StrObservaciones { get; set; }
     }
 }
