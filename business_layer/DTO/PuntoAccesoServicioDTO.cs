61a215196acac999238f5e94267f0e89fdd75f45
Jhovany Santacruz <jsantacruze@hotmail.com>
2022-03-03 18:57:37 -0500

    Jhovany

--- business_layer/DTO/PuntoAccesoServicioDTO.cs
+++ business_layer/DTO/PuntoAccesoServicioDTO.cs
@@ -8,5 +8,26 @@ namespace business_layer.DTO
         public string DireccionMac { get; set; }
         public string Marca { get; set; }
         public string Observaciones { get; set; }
+
+        public string IdtipoEquipo { get; set; }
+        public string Idubicacion { get; set; }
+        public int? TorreId { get; set; }
+        public int? ServidorId { get; set; }
+        public int? EquipoMaxClientes { get; set; }
+        public string EquipoIp { get; set; }
+        public string EquipoSsid { get; set; }
+        public string EquipoSsidPassword { get; set; }
+        public string EquipoUserAdmin { get; set; }
+        public string EquipoUserAdminPassword { get; set; }
+        public string EquipoFrecuencia { get; set; }
+        public string EquipoModo { get; set; }
+        public int? EquipoNumSuscriptores { get; set; }
+
+        public TipoEquipoDTO IdtipoEquipoNavigation { get; set; }
+        public UbicacionEnlaceDTO IdubicacionNavigation { get; set; }
+        public ServidorDTO Servidor { get; set; }
+        public TorreDistribucionDTO Torre { get; set; }
+
+
     }
 }
