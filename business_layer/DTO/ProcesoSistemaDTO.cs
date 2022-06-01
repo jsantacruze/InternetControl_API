68bf88ae1160d1cffe0914a53b0250e7c157b5ad
JULIO JHOVANY SANTACRUZ ESPINOZA <jsantacruze@hotmail.com>
2022-05-31 21:51:12 -0500

    Jhovany

---
+++ business_layer/DTO/ProcesoSistemaDTO.cs
@@ -0,0 +1,8 @@
+namespace business_layer.DTO
+{
+    public class ProcesoSistemaDTO
+    {
+        public string Idproceso { get; set; }
+        public string DescripcionProceso { get; set; }
+    }
+}
